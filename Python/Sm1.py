b81504e62563cd74326577597a6ba1ff0991d674
samuelwp <samuelferreira0071158@gmail.com>
2024-03-01 17:03:10 -0300

    deleted:    produtos.db

---
+++ Python/Sm1.py
@@ -0,0 +1,33 @@
+#teste 1
+'''nome = input('digite seu nome:')
+print('Olá',nome,'! seja bem vindo')'''
+
+#teste 2
+'''idade = input('digite sua idade:')
+peso = input('seu peso:')
+print('seu nome é:',nome)
+print('sua idade é:',idade)
+print('seu peso é:',peso)'''
+
+#data de nascimento basico
+'''Dia = input('digite o dia de nascimento:')
+mes = input('digite o mes de nascimento:')
+ano = input('digite o ano de nascimento:')
+
+print('voce nasceu exatamente no dia',Dia,'de',mes,'de',ano,'. correto?')'''
+
+#Media de 4 notas
+'''nota1 = float(input('Digite a 1° nota:'))
+nota2 = float(input('Digite a 2° nota:'))
+nota3 = float(input('Digite a 3° nota:'))
+nota4 = float(input('Digite a 4° nota:'))
+
+n = (nota1 + nota2 + nota3 + nota4) / 4
+
+print('Sua media é:{}' .format(n) )'''
+
+#Conversao de metro pra cm
+'''metro = float(input('Digite sua medida em metros:'))
+n = (metro) * 100
+print('Sua medida em Cm é: {}'.format(n))'''
+
