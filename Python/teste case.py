519c19dd9d3e7fd532154ca0eb38699967ec665c
samuelwp <samuelferreira0071158@gmail.com>
2024-03-05 15:04:16 -0300

    ,

---
+++ Python/teste case.py
@@ -0,0 +1,12 @@
+dia = 3
+
+match dia:
+    case 1:
+        print("segunda")
+    case 2:
+        print("terça")
+    case 3:
+        print("quarta")
+    case 4:
+        print("sabado")
+        
